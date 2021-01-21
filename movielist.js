dfafaf56a62aba66bacf5cdeb4a410ad2559da2a
Queje <jeremyquerne@hotmail.fr>
2021-01-21 18:50:52 +0100

    pop up working adding style.css and readme

--- movielist.js
+++ movielist.js
@@ -1,10 +1,15 @@
 const searchButton = document.getElementById('search-button');
 const searchInput = document.getElementById('search-input');
 const displaydiv = document.getElementById("display");
-const OMDbAPIbase = "http://www.omdbapi.com/?apikey=bb9280f"
+const OMDbAPIbase = "https://www.omdbapi.com/?apikey=bb9280f"
 
 let Movielist;
 let OMDbAPI;
+let modal;
+let btn;
+let span;
+let modalcontent;
+let databyID;
 
 function changeAPi(input) {
     OMDbAPI = OMDbAPIbase+"&s="+input
@@ -24,13 +29,15 @@ function printDate(i) {
 
 function newDisplay(i){
     displaydiv.innerHTML+=`
-    <div class="card" style="width: 25%;">
+    <div data-aos="fade-down-right" class="card" style="width: 25%;">
         <img id="image${i}">
         <div class="card-body">
             <h5 id="title${i}" class="card-title"></h5>
             <p id="date${i}" class="card-text"></p>
+            <button onclick="popup(${i})" id="More-button${i}" class="btn btn-outline-primary my-2 my-sm-0" type="submit">Read More</button>
         </div>
-    </div>`
+    </div>
+    <br>`
     printPoster(i);
     printTitle(i);
     printDate(i);
@@ -54,6 +61,57 @@ function getMovielist(OMDbAPI) {
     })
 }
 
+// open and display the popup than close the popup
+function popup(i) {
+    modal = document.getElementById("myModal");
+    modal.style.display = "block";
+
+    modalcontent = document.getElementById("modalcontent")
+    modalcontent.innerHTML=`
+        <img id="imagepop${i}">
+        <div id="pop${i}" class="card-body">
+            <h5 id="titlepop${i}" class="card-title"></h5>
+            <p id="datepop${i}" class="card-text"></p>
+            <br>
+            <h6>short plot:</h6>
+            <p id="plot${i}"></p>
+            <br>
+            <h6>Actors:</h6>
+            <p id="actors${i}"></p>
+        </div>`;
+    document.getElementById(`imagepop${i}`).src = Movielist.Search[i].Poster
+    document.getElementById(`pop${i}`).style.color = "black" 
+
+    const titlepopup = document.getElementById(`titlepop${i}`)
+    titlepopup.innerHTML = Movielist.Search[i].Title
+    const datepopup = document.getElementById(`datepop${i}`)
+    datepopup.innerHTML = Movielist.Search[i].Year
+
+    let OMDbAPIID = "https://www.omdbapi.com/?apikey=bb9280f&i="+Movielist.Search[i].imdbID
+
+    fetch(OMDbAPIID, {
+        method: "GET"
+    })
+
+    .then((response) => response.json())
+    .then((item) => {
+        databyID = item
+        console.log(databyID);
+
+        let plotpopup = document.getElementById(`plot${i}`);
+        plotpopup.innerHTML = databyID.Plot
+
+        let actorspopup = document.getElementById(`actors${i}`);
+        actorspopup.innerHTML = databyID.Actors
+    })
+}
+
+function closePopup() {
+    span = document.getElementsByClassName("close")[0];
+    modal.style.display = "none";
+}
+
+// starting function with the click on the search bar
 searchButton.addEventListener('click', (e) => {
     e.preventDefault();
     const inputValue = searchInput.value;
@@ -62,3 +120,23 @@ searchButton.addEventListener('click', (e) => {
     getMovielist(OMDbAPI);
 });
 
+
+// intersection observer to make the animation works with AOS
+let options = {
+    root: null, // relative to document viewport 
+    rootMargin: '0px', // margin around root. Values are similar to css property. Unitless values not allowed
+    threshold: 1.0 // visible amount of item shown in relation to root
+};
+
+function onChange(changes, observer) {
+    changes.forEach(change => {
+        if (change.intersectionRatio > 0) {
+            // your observer logic
+        }
+    });
+}
+
+let observer = new IntersectionObserver(onChange, options);
+let cards = document.querySelectorAll(".card");
+cards.forEach(card => observer.observe(card));
+ 
